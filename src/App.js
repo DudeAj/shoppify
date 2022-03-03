5c5b8f5916cfbab77f5fb39ca47de972abd4d964
sakshib-16 <smakdolls16@gmail.com>
2022-03-03 12:09:24 +0000

    created progress bar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Details from './Components/Details/Details';
 import Main from './Components/Main/Main';
 import Sidebar from './Components/Sidebar/Sidebar';
+import Charts from './Components/Sidebar/Charts/Charts';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <Main />
+      <Main/>
       <Details />
     </div>
   );
