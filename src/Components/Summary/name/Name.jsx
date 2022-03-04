3323a668a9038af023e1e5c7d3a137843e984b6f
gourav jha <gouravjha019@gmai.com>
2022-03-04 10:13:05 +0530

    summary changes

---
+++ src/Components/Summary/name/Name.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./name.module.css";
+
+const Name = () => {
+  return <div className={classes.name}>name 
+ <spam> Avacado </spam>
+  </div>;
+};
+
+export default Name;
