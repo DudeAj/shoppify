d8387af8153f36fd931c65cf5434714a8244c688
Stuti Saxena <stutisaxena0108@gmail.com>
2022-03-05 13:04:10 +0530

    added new changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-
-import './App.css';
-import Details from './Components/Details/Details';
-import ShoppingHistory from './Components/Sidebar/Shopping-History/ShoppingHistory';
-import'./App.css'
-import Sidebar from './Components/Sidebar/Sidebar';
-import {Switch,Route,} from "react-router-dom";
-import Main from './Components/Main/Main'
+import "./App.css";
+import Details from "./Components/Details/Details";
+import ShoppingHistory from "./Components/Sidebar/Shopping-History/ShoppingHistory";
+import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import { Switch, Route } from "react-router-dom";
+import Main from "./Components/Main/Main";
 
 function App() {
   return (
     <div className="App">
-      {/* <Sidebar />
-     
-     <ShoppingHistory
-     />
-    
-      <Details />
- */}
-
-  <Sidebar/>
+      <Sidebar />
       <Switch>
-           <Route exact path= "/" component={Main}/>
-          
-            <Route path="/shoppinghistory" component={ShoppingHistory} />
-           
-          </Switch>
-         <Details/>
+        <Route exact path="/" component={Main} />
+        <Route path="/shoppinghistory" component={ShoppingHistory} />
+      </Switch>
+      <Details />
     </div>
   );
 }
