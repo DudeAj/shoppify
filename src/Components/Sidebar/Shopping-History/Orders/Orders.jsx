049a091a5b88d58cbb733a8c6cb8092ed7873d9f
DudeAj <ajaygangwar04@gmail.com>
2022-03-07 17:50:44 +0530

    fixed some otherissues

--- src/Components/Sidebar/Shopping-History/Orders/Orders.jsx
+++ src/Components/Sidebar/Shopping-History/Orders/Orders.jsx
@@ -5,7 +5,8 @@ import Category from './Category/Category';
 import { useSelector, useDispatch } from 'react-redux';
 import { useState, useEffect } from 'react';
 import { getCategory, LoadItems, setCart } from '../../../../store/actions/';
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory, useParams, Link } from 'react-router-dom';
+import { KeyboardBackspace, EventNote } from '@mui/icons-material';
 
 const Main = () => {
     const param = useParams();
@@ -26,10 +27,16 @@ const Main = () => {
     if (!items || items.length === 0) {
         return <div>Loading</div>
     }
+    const newdate = new Date(items.time)
+    console.log(items)
 
     return (
         <div className={classes.main}>
-            <Topbar />
+            <div className={classes.topbar}>
+                <Link to="/" className={classes.back}><KeyboardBackspace />Back</Link>
+                <h1>{items.title}</h1>
+                <p className={classes.date}><EventNote fontSize="small" sx={{ marginRight: "5px" }} />{items.time.split("T")[0]}</p>
+            </div>
             <div className={classes.Elements}>
                 {categories.map(item => {
                     const cartItem = items.info.filter(it => it.category === item.id);
